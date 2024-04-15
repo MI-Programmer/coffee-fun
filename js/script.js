7a4e9cb5de5d8740162576d656109573810339ca
MI-Progammer <ilhamdivine7@gmail.com>
2024-04-15 16:14:18 +0700

    Add Feature shopping-cart

--- js/script.js
+++ js/script.js
@@ -68,7 +68,11 @@ const searchForm = document.querySelector(".search-form");
 
 // Shopping Cart
 const cartBtn = document.querySelector("#shopping-cart-btn");
+const amountCartItem = document.querySelector(".amount-item");
 const shoppingCart = document.querySelector(".shopping-cart");
+const shoppingCartItems = document.querySelector(".shopping-cart-items");
+const amountTotalPrice = document.querySelector("#amount-total-price");
+const orderProductsBtn = document.querySelector("#order-products-btn");
 
 // Modal
 const closeModalButton = document.querySelector(".close-icon");
@@ -79,7 +83,13 @@ const modalContainer = document.querySelector("#modal-container");
 const hm = document.querySelector("#hamburger-menu");
 
 // Products Row
-let productsRow = document.querySelector("#row-products");
+const productsRow = document.querySelector("#row-products");
+
+// Cart Data Object
+const cartData = {};
+dataProducts.forEach(
+  (product) => (cartData[product.noProduct] = { amount: 0, add: false })
+);
 
 // When hamburger menu on click
 document.querySelector("#hamburger-menu").onclick = (e) => {
@@ -131,59 +141,62 @@ document.addEventListener("click", function (e) {
 
 // element star
 const star = `<i class="fa-regular fa-star"></i>`;
-const starFullColor = `<i class="fa-solid fa-star"></i>`;
+const starSolid = `<i class="fa-solid fa-star"></i>`;
 
 // Make element products card
-dataProducts.map((product) => {
-  const starFull = [...Array(product.starNumber)].map((_) => starFullColor);
-  for (let i = starFull.length; i < 5; i++) {
-    starFull.push(star);
-  }
-  const stars = `${starFull[0]}${starFull[1]}${starFull[2]}${starFull[3]}${starFull[4]}`;
+dataProducts.map(
+  ({ starNumber, noProduct, image, title, discPrice, realPrice }) => {
+    const [str1 = star, str2 = star, str3 = star, str4 = star, str5 = star] = [
+      ...Array(starNumber),
+    ].map((_) => starSolid);
+    const stars = str1 + str2 + str3 + str4 + str5;
 
-  productsRow.innerHTML += `<div class="product-card">
+    productsRow.innerHTML += `<div class="product-card">
           <div class="product-icons">
-            <a ><img src="img/svg/shopping-cart.svg"/></a>
-            <a onclick="showModal(${product.noProduct})"><img src="img/svg/eye.svg"/></a>
+            <a onclick="addToCart(${noProduct})"><img src="img/svg/shopping-cart.svg"/></a>
+            <a onclick="showModal(${noProduct})"><img src="img/svg/eye.svg"/></a>
           </div>
           <div class="product-image">
-            <img src="${product.image}" alt="Product ${product.noProssduct}" />
+            <img src="${image}" alt="Product ${noProduct}" />
           </div>
           <div class="product-content">
-            <h3>${product.title}</h3>
+            <h3>${title}</h3>
             <div class="product-stars">
             ${stars}
             </div>
-            <div class="product-price">IDR ${product.discPrice}k <span>IDR ${product.realPrice}k</span></div>
+            <div class="product-price">IDR ${discPrice}k <span>IDR ${realPrice}k</span></div>
           </div>
         </div>`;
-});
+  }
+);
 
 // Show Modal
-function showModal(noProduct) {
-  const modalProduct = dataProducts[noProduct - 1];
-  const starFull = [...Array(modalProduct.starNumber)].map(
-    (_) => starFullColor
-  );
-  for (let i = starFull.length; i < 5; i++) {
-    starFull.push(star);
-  }
-  const stars = `${starFull[0]}${starFull[1]}${starFull[2]}${starFull[3]}${starFull[4]}`;
+function showModal(noProductPara, contentSuccesModal = false) {
+  const { noProduct, image, desc, realPrice, discPrice, starNumber } =
+    dataProducts[noProductPara - 1];
+  const [str1 = star, str2 = star, str3 = star, str4 = star, str5 = star] = [
+    ...Array(starNumber),
+  ].map((_) => starSolid);
+  const stars = str1 + str2 + str3 + str4 + str5;
 
   modalContainer.innerHTML += `<div class="modal-content">
-          <img src="${modalProduct.image}" alt="${modalProduct.noProduct}" />
+          ${
+            contentSuccesModal
+              ? contentSuccesModal
+              : `<img src="${image}" alt="${noProduct}" />
           <div class="product-content">
-            <h3>Product ${modalProduct.noProduct}</h3>
+            <h3>Product ${noProduct}</h3>
             <p>
-              ${modalProduct.desc}
+              ${desc}
             </p>
             <div class="product-stars">
               ${stars}
             </div>
-            <div class="product-price">IDR ${modalProduct.discPrice}k <span>IDR ${modalProduct.realPrice}k</span></div>
-            <a><img src="img/svg/shopping-cart.svg"/> <span>add to cart</span></a
+            <div class="product-price">IDR ${discPrice}k <span>IDR ${realPrice}k</span></div>
+            <a onclick="addToCart(${noProductPara})"><img src="img/svg/shopping-cart.svg"/> <span>add to cart</span></a
             >
-          </div>
+          </div>`
+          }
         </div>`;
 
   itemDetailModal.style.display = "flex";
@@ -194,3 +207,131 @@ function removeModal() {
   itemDetailModal.style.display = "none";
   modalContainer.lastElementChild.remove();
 }
+
+// Add to cart and Remove from Cart
+
+function addToCart(noProduct) {
+  cartData[noProduct].amount = cartData[noProduct].amount + 1;
+
+  makeItemCart(noProduct);
+}
+
+function removeFromCart(noProduct) {
+  cartData[noProduct].amount = cartData[noProduct].amount - 1;
+
+  makeItemCart(noProduct);
+}
+
+function makeItemCart(noProductPara) {
+  dataProducts.forEach(({ noProduct, image, discPrice }) => {
+    if (cartData[noProduct].amount && !cartData[noProduct].add) {
+      shoppingCartItems.innerHTML += `<div class="cart-item" id="cart-item${noProduct}">
+          <img src=${image} alt=${noProduct} s/>
+          <div class="item-detail">
+            <h3>Product ${noProduct}</h3>
+            <div class="item-price">IDR ${discPrice}k</div>
+          </div>
+          <div class="total-item">
+            <div class="amount-item">
+              <div class="cart-btn-amount" onclick="addToCart(${noProduct})">+</div>
+              <div id="amount-cartitem${noProduct}">${cartData[noProductPara].amount}</div>
+              <div class="cart-btn-amount" onclick="removeFromCart(${noProduct})">-</div>
+            </div>
+            <div class="total-price-item" id="total-price-item${noProduct}">${discPrice}K
+            </div>
+          </div>
+        </div>`;
+      orderProductsBtn.style.display = "block";
+
+      totalAmountItem();
+      totalPriceItems();
+      return (cartData[noProduct].add = true);
+    } else if (cartData[noProduct].amount >= 1) {
+      document.querySelector(`#amount-cartitem${noProduct}`).innerHTML =
+        cartData[noProduct].amount;
+      document.querySelector(`#total-price-item${noProduct}`).textContent =
+        cartData[noProduct].amount * discPrice + "K";
+
+      totalAmountItem();
+      totalPriceItems();
+    } else if (!cartData[noProduct].amount && cartData[noProduct].add) {
+      const child = document.getElementById(`cart-item${noProduct}`);
+      shoppingCartItems.removeChild(child);
+
+      if (shoppingCartItems.children.length) {
+        setTimeout(() => {
+          shoppingCart.classList.add("active");
+        }, 200);
+      } else if (!shoppingCartItems.children.length) {
+        orderProductsBtn.style.display = "none";
+      }
+
+      totalAmountItem();
+      totalPriceItems();
+      return (cartData[noProduct].add = false);
+    } else {
+      return;
+    }
+  });
+}
+
+function totalAmountItem() {
+  let totalItem = 0;
+
+  for (const item in cartData) {
+    if (Object.hasOwnProperty.call(cartData, item)) {
+      const { amount } = cartData[item];
+      if (amount) {
+        totalItem += amount;
+      }
+    }
+  }
+
+  amountCartItem.textContent = totalItem;
+}
+
+function totalPriceItems() {
+  let totalPrice = 0;
+
+  dataProducts.forEach(({ noProduct, discPrice }) => {
+    if (cartData[noProduct].amount) {
+      totalPrice += cartData[noProduct].amount * discPrice;
+    }
+  });
+
+  amountTotalPrice.textContent = totalPrice > 0 ? totalPrice + "K" : "0";
+}
+
+// Click order product btn
+orderProductsBtn.addEventListener("click", () => {
+  const totalPriceOrdered = amountTotalPrice.textContent;
+  const totalAmountOrdered = amountCartItem.textContent;
+  let imgOrdered = "";
+  dataProducts.forEach(({ noProduct, image }) => {
+    if (cartData[noProduct].amount) {
+      imgOrdered += `<img src="${image}" class="image-ordered" style="height: 7rem;"/>`;
+    }
+  });
+
+  const elementSuccesModal = `<div class="modal-ordered">
+                                <h1>Success <i class="fa-solid fa-circle-check"></i></h1>
+                                <h2>You have ordered ${totalAmountOrdered} products at a price of ${totalPriceOrdered}</h2>
+                                <div class="images-modal-ordered">
+                                  ${imgOrdered}
+                                </div>
+                                <p>Thank you for ordering. We hope you have a pleasant day</p>
+                              </div>`;
+
+  dataProducts.forEach(({ noProduct }) => {
+    cartData[noProduct].amount = 0;
+    cartData[noProduct].add = false;
+  });
+
+  shoppingCartItems.innerHTML = "";
+  amountCartItem.textContent = 0;
+  amountTotalPrice.textContent = 0;
+  orderProductsBtn.style.display = "none";
+  setTimeout(() => {
+    showModal(1, elementSuccesModal);
+  }, 1000);
+});
